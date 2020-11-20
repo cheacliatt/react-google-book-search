250e0e26e1d8b50bf73935dc8083c84f744ab9fa
CheaC <cheacliatt@gmail.com>
2020-11-19 20:41:36 -0500

    Updated database

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -14,7 +14,7 @@ router.post("/api/books", function (req, res) {
   });
 });
 
-router.delete("/api/books/:id", function (req, res) {
+router.delete("/:id", function (req, res) {
   books.deleteOne({ _id: req.params.id }, req.body).then((bookData) => {
     res.json(bookData);
   });
